565fc10fd484afe4a518780eafac66877287a4f6
vairakrishna <krish4july96@gmail.com>
2017-09-30 20:06:31 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int n,p,power;
+printf("Enter the number to which  the power is to be calculated");
+scanf("%d",&n);
+printf("Enter the power to be calculated");
+scanf("%d",&p);
+power=n^p;
+printf("Power of the given number is %d",power);
+}
