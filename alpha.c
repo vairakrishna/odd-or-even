d4378d8e0a3f46d29933a5de69e042ac0ca7ee5b
vairakrishna <krish4july96@gmail.com>
2017-09-30 19:34:31 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+char c,y='A'z='a';
+int i;
+printf("Enter a character within single quotes");
+scanf("%c",&c);
+for(i=o;i<25;i++)
+{
+if(c==z||c==y)
+{
+printf("Entered value is an alphabet");
+}
+y++;
+z++;
+}
+}
