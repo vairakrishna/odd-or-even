8e5cf27b7ead8f1b287c17987bd2ffb708457afc
vairakrishna <krish4july96@gmail.com>
2017-09-30 19:47:50 +0530

    Create digits.c

---
+++ digits.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("Enter a number");
+if((a/2)>4)
+{
+printf("given values is a two digit number");
+}
+if((a/2)>49)
+{
+printf("Entered value is a three digit number");
+}
+}
